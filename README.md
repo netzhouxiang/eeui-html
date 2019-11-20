a013cbcb6e774c9f99784e5f4b9647fb8cfefe34
netzhouxiang <200592114@qq.com>
2019-11-20 17:08:32 +0800

    eeui-html

---
+++ README.md
@@ -0,0 +1,38 @@
+# eeui-html
+解析eeui html标签
+
+当前支持 em u s strong p img标签；支持背景色字体颜色，对齐方向； 在组件内可自行扩展
+
+实现思路：通过html2json转换接口返回的HTML为JSON，循环JSON处理想要的标签，转化为对应数据，进行循环绑定；
+参考链接：https://blog.csdn.net/codingandroid/article/details/77451389
+
+# 使用方法：
+1.下载压缩包,解压到eeui/src目录
+2.然后在项目引入后，调用即可，如下：
+    import html from "../../components/html.vue";
+    <html :content="v.content"></html>
+
+# 如果是使用VUE开发后台，建议使用配合vue-quill-editor编辑器使用，配置:
+    editorOption: {
+      modules: {
+        toolbar: [
+          ["bold", "italic", "underline", "strike"],
+          [{ color: [] }, { background: [] }],
+          [{ align: [] }],
+          ["image"],
+          ["clean"]
+        ]
+      }
+    },
+
+# html.vue 组件内样式，分别对应vue-quill-editor编辑器 （toolbar：align） 的对齐方向，可自行调整：
+.ql-align-center{
+  justify-content: center;
+}
+.ql-align-right{
+  justify-content: flex-end;
+}
+.ql-align-justify{
+  justify-content: space-between;
+}
+
---
+++ components/html.vue
@@ -0,0 +1,168 @@
+<template>
+    <div>
+      <div v-for="(child, index) in children" :key="index">
+        <div v-if="child.tag==='div'" :class="child.class">
+          <template v-for="(item,y) in child.texts">
+            <text v-if="item.tag==='text'" :key="y" :style="item.style">{{item.text}}</text>
+            <image v-if="item.tag==='image'" :key="y" @load="onImageLoad" :src="item.src" resize="stretch" 
+            class="product-image" :style="{opacity:0,width: '2000px', height: '2000px'}"></image>
+            <text v-if="item.tag==='br'" :key="y" class="br"></text>
+          </template>
+        </div>
+      </div>
+    </div>
+</template>
+<style>
+.p{
+  flex-direction:row;
+  flex-wrap:wrap;
+}
+.br {
+  height: 20px;
+}
+.ql-align-center{
+  justify-content: center;
+}
+.ql-align-right{
+  justify-content: flex-end;
+}
+.ql-align-justify{
+  justify-content: space-between;
+}
+</style>
+<script>
+  const eeui = app.requireModule('eeui');
+  const modal = weex.requireModule('modal');
+  const animation = weex.requireModule('animation');
+  var {html2json} = require('../html2json/html2json');
+  /*
+    weex html解析 十年 200592114
+    当前支持 em u s strong p img标签；支持背景色字体颜色，对齐方向；
+    配合VUE vue-quill-editor编辑器使用
+    editorOption: {
+      modules: {
+        toolbar: [
+          ["bold", "italic", "underline", "strike"],
+          [{ color: [] }, { background: [] }],
+          [{ align: [] }],
+          ["image"],
+          ["clean"]
+        ]
+      }
+    },
+  */
+  export default {
+    props: {
+      //内容
+      content: {
+        type: String,
+        required: true
+      },
+      //图片最大宽度 自动等比
+      imgWidth: {
+        type: Number,
+        required: true,
+        default: 690
+      }
+    },
+    mounted() {},
+    computed: {
+      children() {
+        if(this.content){
+          const json = html2json(this.content,this);
+          //this.$alert(JSON.stringify(json))
+          return this.translateJSON(json)
+        }
+        return [];
+      }
+    },
+    methods: {
+      onImageLoad (event) {
+        if (event.success) {
+            eeui.getImageSize(event.target.attr.src, result=>{
+              var _w=result.width;
+              var _h=result.height;
+              if(_w>this.imgWidth){
+                var bi = this.imgWidth / _w;
+                _h = result.height * bi;
+                _w = this.imgWidth;
+              }
+              animation.transition(event.target,{
+                styles:{
+                  width: _w+"px",
+                  height: _h+"px",
+                  opacity:1
+                },
+                duration: 0,
+                timingFunction:'ease',
+                delay: 0,
+                needLayout:true,
+                }, function(){})
+            })
+          }
+        },
+        translateJSON(node) {
+          const result = [];
+          var _self=this;
+          const translate = (_texts, node, styleList) => {
+            var styleL = styleList;
+            if(node.attr && node.attr.style){
+              var css_arr = node.attr.style.split(";")
+              css_arr.forEach(item=>{
+                var css = item.split(":")
+                styleL[css[0]] = css[1];
+              });
+            }
+            if (node.tag && node.tag === "img") {
+              _texts.push({
+                tag: "image",
+                src: node.attr.src
+              });
+            } else if (node.tag && node.tag === "br") {
+              _texts.push({
+                tag: "br"
+              });
+            } else if (node.tag && node.tag === "u") {
+              styleL['text-decoration'] = 'underline';
+            } 
+            else if (node.tag && node.tag === "em") {
+              styleL['font-style'] = 'italic';
+            } 
+            else if (node.tag && node.tag === "s") {
+              styleL['text-decoration'] = 'line-through';
+            }  
+            else if (node.tag && node.tag === "strong") {
+              styleL.fontWeight = 'bold';
+            } else if (!node.tag && node.node === "text") {
+              const style = styleL
+              _texts.push({
+                tag: "text",
+                style,
+                text: node.text.replace(/&[a-z]{3,4};/g, "")
+              });
+            }
+            if (node.child && node.child.length) {
+              node.child.forEach(c => translate(_texts, c, styleL));
+            }
+          };
+          //处理P标签
+          node.child.forEach(item => {
+            var _texts = [];
+            if (item.child && item.child.length) {
+              item.child.forEach(c => translate(_texts, c, {}));
+            }
+            var _class = ["p"];
+            if(item.attr && item.attr.class){
+              _class.push(item.attr.class);
+            }
+            result.push({
+              tag: "div",
+              class: _class,
+              texts: _texts
+            });
+          });
+          return result;
+      }
+    }
+  }
+</script>
